7c1155a8c8f7386a591ada4c858b247a3500039f
Ugnius <ugniusnaujokas@gmail.com>
2019-12-24 13:10:25 +0200

    Completed App1

---
+++ App1/App1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String vardas = "Ugnius";
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine(vardas);
+            }
+            Console.Read();
+        }
+    }
+}
