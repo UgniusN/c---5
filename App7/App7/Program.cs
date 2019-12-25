b27d81f44001f15e981782bcc5e34d83c18bfdef
Ugnius <ugniusnaujokas@gmail.com>
2019-12-25 09:06:19 +0200

    Completed App7

--- App7/App7/Program.cs
+++ App7/App7/Program.cs
@@ -10,6 +10,12 @@ namespace App7
     {
         static void Main(string[] args)
         {
+            int[] masyvas = { 5, 4, 2, 4, 5 };
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine(masyvas[i]);
+            }
+            Console.Read();
         }
     }
 }
