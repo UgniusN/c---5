f1675204c93db11214c12294e76cd9575972469e
Ugnius <ugniusnaujokas@gmail.com>
2019-12-25 09:10:11 +0200

    Completed App9

---
+++ App9/App9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masyvas = { 5, 4, 2, 4, 5 };
+            int suma = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                suma += masyvas[i];
+            }
+            Console.WriteLine(suma);
+            Console.Read();
+        }
+    }
+}
