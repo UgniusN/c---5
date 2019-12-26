329067cfb45beaf96bdfc73d55208cb52599d1f4
Ugnius <ugniusnaujokas@gmail.com>
2019-12-26 10:49:15 +0200

    Completed App11

--- App13/App11/Program.cs
+++ App13/App11/Program.cs
@@ -10,6 +10,23 @@ namespace App11
     {
         static void Main(string[] args)
         {
+            int[] skaiciai = { 4, 2, 5 };
+            int[] skaiciaiDu = { 9, 1, 5 };
+            int[] skaiciaiTrys = new int[6];
+            for(int i=0; i<skaiciai.Length; i++)
+            {
+                skaiciaiTrys[i] = skaiciai[i];
+            }
+            for(int i=0; i<3; i++)
+            {
+                skaiciaiTrys[i+3] = skaiciaiDu[i];
+            }
+
+            for(int i=0; i<6; i++)
+            {
+                Console.WriteLine(skaiciaiTrys[i]);
+            }
+            Console.Read();
         }
     }
 }
