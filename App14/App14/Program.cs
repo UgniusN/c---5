4603d0e951515a4f75c4d0e0732460617e311213
Ugnius <ugniusnaujokas@gmail.com>
2019-12-30 04:11:27 +0200

    Completed App14

--- App14/App14/Program.cs
+++ App14/App14/Program.cs
@@ -10,6 +10,25 @@ namespace App14
     {
         static void Main(string[] args)
         {
+            String[] vardaiPradinis = { "Ugnius", "Rimas", "Andrius", "Tomas", "Gvidas", "Daniele", "Robertas", "Domantas", "Antanas", "Jonas" };
+            String[] vardaiAntras = new String[20];
+
+            int skaicius = 0;
+            int vardoPozicija = 0;
+            foreach(string vardas in vardaiPradinis)
+            {
+                if (skaicius == 4 || skaicius == 6)
+                {
+                    skaicius++;
+                    continue;
+                }
+                vardaiAntras[vardoPozicija] = vardaiPradinis[skaicius];
+                skaicius++;
+                vardoPozicija++;
+                Console.WriteLine("{0} {1} {2}",skaicius,vardoPozicija,vardas);
+            }
+
+            Console.Read();
         }
     }
 }
